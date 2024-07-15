928256a692ca8f00bfee65ae84dd82055244645e
JSlee_Home <jslee7518@gmail.com>
2024-07-16 07:40:08 +0900

    test at home

---
+++ testCode/src/testString/charAtTest/CharAtTest.java
@@ -0,0 +1,28 @@
+package testString.charAtTest;
+
+import java.util.Arrays;
+
+
+public class CharAtTest {
+	@SuppressWarnings("unused")
+	public static void main(String[] args) {
+		String a = "hello";
+//		a.charAt(0);
+//		a.concat(a);
+		int[] arrInt = {1, 3, 5};
+		
+		Arrays.asList(1, 3, "", new CharAtTest());
+		
+		
+//		List<Integer> listInt = Arrays.asList(0);
+//		for (int i : arrInt) {
+//			listInt.add(i);
+//		}
+		
+		
+		
+		
+		
+		
+	}
+}
